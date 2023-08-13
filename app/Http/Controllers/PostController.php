55923d26d635eb3853547bc95aeeb8aaf84838aa
potato3206 <potato3206@gmail.com>
2023-08-13 22:15:10 +0900

    8-5

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -9,17 +9,14 @@ class PostController extends Controller
 {
     public function index(Post $post)
     {
-        return view('posts.index')->with(['posts' => $post->getPaginateByLimit()]);
+        return view('posts.index')->with(['posts' => $post->getPaginateByLimit(5)]);
     }
     
     public function show(Post $post)
     {
         return view('posts/show')->with(['post' => $post]);
     }
-    public function show (Post $post)
-    {
-        return view('posts/show')->with(['post' => $post]);
-    }
+    
     public function create()
     {
         return view('posts/create');
@@ -28,6 +25,17 @@ class PostController extends Controller
     {
         $input = $request['post'];
         $post->fill($input)->save();
+        return redirect('/posts/' . $post->id);
+    }
+    
+    public function edit(Post $post)
+    {
+        return view('posts.edit')->with(['post' => $post]);
+    }
+    public function update(PostRequest $request, Post $post)
+    {
+        $input_post = $request['post'];
+        $post->fill($input_post)->save();
         return redirect('/posts/' . $post->id);
     }
 }
